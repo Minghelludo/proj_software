7c157648e6ada148e17a809ebc65d901fd5a3cd3
Breno Minghelli <brenominghelli@gmail.com>
2024-05-08 17:08:40 -0300

    botao funcionando

--- scenes/tabuleiro.gd
+++ scenes/tabuleiro.gd
@@ -1,18 +1,27 @@
 extends Node2D
 var teste
 @onready var p1 = $player1
-@onready var display = $Label
+@onready var display_p1 = $Label_p1
 @onready var c_inic = $casa_inicio
+
+var d1
+var rng
+
+func att_dinheiro():
+	d1 = p1.get_dinheiro()
+	display_p1.mostra_di(d1)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	teste = p1.get_dinheiro()
-	display.mostra_di(teste)
+	d1 = p1.get_dinheiro()
+	display_p1.mostra_di(d1)
+	$dice_roll.pressed.connect(func():
+		rng = $dice.roll()
+		p1.move_casa(rng))
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	p1.move_casa(1)
-	teste=p1.get_dinheiro()
-	display.mostra_di(teste)
+	att_dinheiro()
 	pass
