abbc97af54fdb31a3d0a0f1798b4436211e15fef
Breno Minghelli <brenominghelli@gmail.com>
2024-05-10 14:32:19 -0300

    dice label

---
+++ scenes/Label_dice.gd
@@ -0,0 +1,12 @@
+extends Label
+
+func mostra_dice(di):
+	text = ("Dados: ")+str(di)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- scenes/tabuleiro.gd
+++ scenes/tabuleiro.gd
@@ -6,6 +6,7 @@ extends Node2D
 @onready var p2 = $player2
 @onready var display_p1 = $Label_p1
 @onready var display_p2 = $Label_p2
+@onready var display_dice = $Label_dice
 @onready var c_inic = $casa_inicio
 
 #variaveis usadas d1 é o display do p1
@@ -19,6 +20,9 @@ func att_dinheiro():
 	display_p1.mostra_di(d1)
 	d2 = p2.get_dinheiro()
 	display_p2.mostra_di(d2)
+#atualiza na tela os dados rolados
+func att_dice(dice_aux):
+	display_dice.mostra_dice(dice_aux)
 	
 func passa_turno(pl):
 	if pl == p1:
@@ -33,6 +37,7 @@ func _ready():
 	#sinal do botao pressionado pra mover
 	$dice_roll.pressed.connect(func():
 		rng = $dice.roll()
+		att_dice(rng)
 		pl_atual.move_casa(rng)
 		pl_atual = passa_turno(pl_atual))
 	pass # Replace with function body.
